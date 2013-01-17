bd5ef86dd19b4c6bcb79e55fd9cb5f9b8fa961d9
Andrea Reginato <andrea.reginato@gmail.com>
2013-01-17 16:10:14 +0100

    Set the access token on initialization as it is a one time configuration

--- test/devices.js
+++ test/devices.js
@@ -1,20 +1,19 @@
 var config  = {
-  'secrets' : { 'clientId' : '-', 'clientSecret' : '-', 'redirectUrl' : '-' },
   'lelylan' : {
     'oauthAuthorizationUri' : 'http://localhost:3000/oauth/authorization',
     'oauthTokenUri' : 'http://localhost:3000/oauth/token',
     'apiUri' : 'http://localhost:8000'
   },
+  'token': '42cf98a147a8973e682b91337abfaf53eca64336767dc8fec5f7fb8f11b8499b'
 }
 
-var token = '42cf98a147a8973e682b91337abfaf53eca64336767dc8fec5f7fb8f11b8499b'
 var Lelylan = require('./../lib/lelylan-node.js')(config);
 var response, error;
 
 describe('GET all devices',function() {
 
   before(function(done) {
-    Lelylan.Devices.all(token, function(e, r) {
+    Lelylan.Devices.all(function(e, r) {
       error = e; response = r; done();
     })
   })
