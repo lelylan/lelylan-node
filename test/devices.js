015620c637a7513e152f3718e10017f9c375855a
Andrea Reginato <andrea.reginato@gmail.com>
2013-01-17 19:20:03 +0100

    Added capability to accept params to /devices

--- test/devices.js
+++ test/devices.js
@@ -2,25 +2,54 @@ var Lelylan = require('./../lib/lelylan-node.js')({ 'token': '5f7fb8f11b8499b' }
 var nock    = require('nock');
 var request, response, error;
 
-describe('GET all devices',function() {
+describe('Lelylan.Devices',function() {
 
-  before(function(done) {
-    request = nock('http://api.lelylan.com').get('/devices').replyWithFile(200, __dirname + '/fixtures/devices.json');
-    done();
-  })
+describe('#all',function() {
 
-  before(function(done) {
-    Lelylan.Devices.all(function(e, r) {
-      error = e; response = r; done();
+  describe('with no search params', function() {
+
+    beforeEach(function(done) {
+      request = nock('http://api.lelylan.com').get('/devices').replyWithFile(200, __dirname + '/fixtures/devices.json');
+      done();
     })
+
+    beforeEach(function(done) {
+      Lelylan.Devices.all({}, function(e, r) {
+        error = e; response = r; done();
+      })
+    })
+
+    it('makes the HTTP request', function() {
+      request.isDone();
+    });
+
+    it('return a json array',function() {
+      response.should.be.a('object');
+    });
   })
 
-  it('makes the HTTP request', function() {
-    request.isDone();
-  });
+  describe('with search params', function() {
 
-  it('return a json array',function() {
-    response.should.be.a('object');
-  });
+    beforeEach(function(done) {
+      request = nock('http://api.lelylan.com').get('/devices', {'per': 10}).replyWithFile(200, __dirname + '/fixtures/devices.json');
+      done();
+    })
+
+    beforeEach(function(done) {
+      Lelylan.Devices.all({per: 10}, function(e, r) {
+        error = e; response = r; done();
+      })
+    })
+
+    it('makes the HTTP request', function() {
+      request.isDone();
+    });
+
+    it('return a json array',function() {
+      response.should.be.a('object');
+    });
+  })
 });
+
+})
 
