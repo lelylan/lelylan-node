7bf60cb2cb26a3582fa9dbd69bf8de2284bf4f47
Andrea Reginato <andrea.reginato@gmail.com>
2013-01-18 09:37:14 +0100

    Added histories services

---
+++ test/histories.js
@@ -0,0 +1,77 @@
+var Lelylan = require('./../lib/lelylan-node.js')({ 'token': '5f7fb8f11b8499b' });
+var nock    = require('nock');
+var request, response, error;
+
+describe('Lelylan.Histories',function() {
+
+  describe('#find',function() {
+
+    beforeEach(function(done) {
+      request = nock('http://api.lelylan.com').get('/histories/1').replyWithFile(200, __dirname + '/fixtures/history.json');
+      done();
+    })
+
+    beforeEach(function(done) {
+      Lelylan.Histories.find('1', function(e, r) {
+        error = e; response = r; done();
+      })
+    })
+
+    it('makes the HTTP request', function() {
+      request.isDone();
+    });
+
+    it('return a json array',function() {
+      response.should.be.a('object');
+    });
+  });
+
+
+  describe('#all',function() {
+
+    describe('with no search params', function() {
+
+      beforeEach(function(done) {
+        request = nock('http://api.lelylan.com').get('/histories').replyWithFile(200, __dirname + '/fixtures/histories.json');
+        done();
+      })
+
+      beforeEach(function(done) {
+        Lelylan.Histories.all({}, function(e, r) {
+          error = e; response = r; done();
+        })
+      })
+
+      it('makes the HTTP request', function() {
+        request.isDone();
+      });
+
+      it('return a json array',function() {
+        response.should.be.a('object');
+      });
+    });
+
+    describe('with search params', function() {
+
+      beforeEach(function(done) {
+        request = nock('http://api.lelylan.com').get('/histories?per=10').replyWithFile(200, __dirname + '/fixtures/histories.json');
+        done();
+      })
+
+      beforeEach(function(done) {
+        Lelylan.Histories.all({'per': 10}, function(e, r) {
+          error = e; response = r; done();
+        })
+      })
+
+      it('makes the HTTP request', function() {
+        request.isDone();
+      });
+
+      it('return a json array',function() {
+        response.should.be.a('object');
+      });
+    });
+  });
+})
+
