7487981742ab935b0cea694494215f53841238b6
Andrea Reginato <andrea.reginato@gmail.com>
2013-01-22 11:57:39 +0100

    Updated all tests to use the token created by simple-oauth2

--- test/errors.js
+++ test/errors.js
@@ -1,5 +1,12 @@
-var Lelylan = require('./../lib/lelylan-node.js')({ 'token': '5f7fb8f11b8499b' });
+var oauth_credentials = { client: { id: 'client-id', secret: 'client-secret', site: 'http://people.lelylan.com' } };
+var json_token        = { 'access_token': '4adc339e06c', 'refresh_token': 'ec1a59d2', 'expires_in': 7200 };
+
+var OAuth2 = require('simple-oauth2')(oauth_credentials);
+var token  = OAuth2.AccessToken.create(json_token);
+
+var Lelylan = require('./../lib/lelylan-node.js')({ 'token': token});
 var nock    = require('nock');
+
 var request, response, error;
 
 describe('Lelylan Error',function() {
