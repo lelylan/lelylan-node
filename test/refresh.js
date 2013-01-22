0d5a12a6d4e54681e8c95ee1d253b008e7672c03
Andrea Reginato <andrea.reginato@gmail.com>
2013-01-22 13:09:09 +0100

    Added support to refresh access token when expired

---
+++ test/refresh.js
@@ -0,0 +1,64 @@
+var oauth_credentials = { client: { id: 'client-id', secret: 'client-secret', site: 'http://people.lelylan.com' } };
+var json_token        = { 'access_token': '4adc339e06c', 'refresh_token': 'ec1a59d2', 'expires_in': 7200 };
+
+var OAuth2 = require('simple-oauth2')(oauth_credentials);
+var token  = OAuth2.AccessToken.create(json_token);
+
+var Lelylan = require('./../lib/lelylan-node.js')({ 'token': token});
+var nock    = require('nock');
+
+var request, response, error;
+
+describe('Lelylan API call',function() {
+
+  describe('when access token is valid',function() {
+
+    beforeEach(function(done) {
+      request = nock('http://api.lelylan.com').get('/devices/1').replyWithFile(200, __dirname + '/fixtures/device.json');
+      done();
+    })
+
+    beforeEach(function(done) {
+      Lelylan.Device.find('1', function(e, r) {
+        error = e; response = r; done();
+      })
+    })
+
+    it('makes the HTTP request', function() {
+      request.isDone();
+    });
+
+    it('return a json array',function() {
+      response.should.have.property('id');
+    });
+  });
+
+  describe('when access token is expired',function() {
+
+		var api;
+
+    beforeEach(function(done) {
+			Lelylan.config.token.token.expires_at = Date.yesterday();
+			var params = { 'grant_type': 'refresh_token', refresh_token: 'ec1a59d2' };
+			request = nock('http://people.lelylan.com').post('/oauth/token', params).replyWithFile(200, __dirname + '/fixtures/access_token.json');
+			done();
+		});
+
+    beforeEach(function(done) {
+      api = nock('http://api.lelylan.com').get('/devices/1').replyWithFile(200, __dirname + '/fixtures/device.json');
+      done();
+    })
+
+    beforeEach(function(done) {
+      Lelylan.Device.find('1', function(e, r) {
+        error = e; response = r; done();
+      })
+    })
+
+    it('makes the HTTP requests', function() {
+      request.isDone(); api.isDone();
+      response.should.have.property('id');
+    });
+  });
+})
+
