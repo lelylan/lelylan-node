a5444602525d2fdd9080df96fcceda38312f66ad
Andrea Reginato <andrea.reginato@gmail.com>
2013-01-23 11:16:03 +0100

    Soft tabs on test/physical.js

--- test/physical.js
+++ test/physical.js
@@ -14,22 +14,22 @@ describe('Lelylan.Physical',function() {
   describe('#properties',function() {
 
     beforeEach(function(done) {
-			var signature = 'b919e6d73fb956335507b809c457cbda0e4856e2';
+      var signature = 'b919e6d73fb956335507b809c457cbda0e4856e2';
       request = nock('http://mqtt.lelylan.com')
-  							.filteringRequestBody(function(path) {return '*'})
-								.put('/physicals/1', '*')
-								.reply(202);
+                .filteringRequestBody(function(path) {return '*'})
+                .put('/physicals/1', '*')
+                .reply(202);
       done();
     })
 
     beforeEach(function(done) {
-			Lelylan.Physical.properties('http://mqtt.lelylan.com/physicals/1', 'secret', {'properties': {}}, function(e, r) {
+      Lelylan.Physical.properties('http://mqtt.lelylan.com/physicals/1', 'secret', {'properties': {}}, function(e, r) {
         error = e; response = r; done();
       })
     })
 
     it('makes the HTTP request', function() {
-			request.isDone();
+      request.isDone();
     });
 
     it('return a json array',function() {
